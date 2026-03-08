0474503f6123a7c1b06bf54a46f47d6462d6bd3b
Haobo Zou <91838141+haobozou@users.noreply.github.com>
2026-03-09 10:44:01 +1100

    refactor: remove unused test dependencies and configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,12 +21,6 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
 
 spotless {
