fdd1fdb1d8fdc076cb3ea13b2c7a103bf0e2b12f
Haobo Zou <91838141+haobozou@users.noreply.github.com>
2026-03-09 11:05:33 +1100

    chore: configure git hooks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
-rootProject.name = 'app'
+rootProject.name = "app"
+
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.1.10"
+}
+
+gitHooks {
+    preCommit {
+        tasks("spotlessCheck", "check")
+    }
+
+    commitMsg {
+        conventionalCommits()
+    }
+
+    createHooks(true)
+}
